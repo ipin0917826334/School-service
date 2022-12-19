11a334614d5e19ef476be864869562b857bcfe9d
Thitiwut85 <73686921+Thitiwut85@users.noreply.github.com>
2022-12-20 01:01:28 +0700

    impl rabbitmq

--- grade-services/src/main/java/com/example/gradeservices/core/data/GradeRepository.java
+++ grade-services/src/main/java/com/example/gradeservices/core/data/GradeRepository.java
@@ -1,10 +1,16 @@
 package com.example.gradeservices.core.data;
 
 import com.example.gradeservices.core.GradeEntity;
+import com.example.gradeservices.query.rest.GradeRestModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GradeRepository extends MongoRepository<GradeEntity, String> {
     GradeEntity findBy_id(String _id);
+    @Query(value = "{ 'studentId' : ?0 }", fields="{'grade':1}")
+    List<GradeRestModel> findByStudentId(String studentId);
 }
--- grade-services/src/main/java/com/example/gradeservices/query/rest/GradeQueryController.java
+++ grade-services/src/main/java/com/example/gradeservices/query/rest/GradeQueryController.java
@@ -1,26 +1,53 @@
 package com.example.gradeservices.query.rest;
 
-import com.example.gradeservices.query.FindGradeQuery;
-import org.axonframework.messaging.responsetypes.ResponseTypes;
+import com.example.gradeservices.core.data.GradeRepository;
 import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/grades")
 public class GradeQueryController {
+    private final GradeRepository gradeRepository;
+
     @Autowired
     QueryGateway queryGateway;
 
-    @GetMapping
-    public List<GradeRestModel> getGrades(){
-        FindGradeQuery findGradeQuery = new FindGradeQuery();
-        List<GradeRestModel> grade = queryGateway
-                .query(findGradeQuery, ResponseTypes.multipleInstancesOf(GradeRestModel.class)).join();
-        return grade;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public GradeQueryController(GradeRepository gradeRepository) {
+        this.gradeRepository = gradeRepository;
     }
+
+    @GetMapping
+    List<GradeRestModel> findGradeByStudentId(@RequestBody GradeRestModel model) throws IOException {
+        List<GradeRestModel> grades = gradeRepository.findByStudentId(model.getStudentId());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(baos);
+
+        for (GradeRestModel element : grades) {
+            out.writeUTF(element.getGrade());
+        }
+        byte[] bytes = baos.toByteArray();
+        rabbitTemplate.convertAndSend("gradeExchange","grade", bytes);
+        return grades;
+    }
+
+//    @GetMapping
+//    public List<GradeRestModel> getGrades(){
+//        FindGradeQuery findGradeQuery = new FindGradeQuery();
+//        List<GradeRestModel> grade = queryGateway
+//                .query(findGradeQuery, ResponseTypes.multipleInstancesOf(GradeRestModel.class)).join();
+//        return grade;
+//    }
 }
