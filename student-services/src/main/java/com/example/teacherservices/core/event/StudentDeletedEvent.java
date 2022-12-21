fe9f2f455c83c0dc7c7cb82e72aac1a36b053afe
Thitiwut85 <73686921+Thitiwut85@users.noreply.github.com>
2022-12-21 19:44:14 +0700

    Delete student-services/src/main/java/com/example/teacherservices directory

--- student-services/src/main/java/com/example/teacherservices/core/event/StudentDeletedEvent.java
+++
@@ -1,8 +0,0 @@
-package com.example.teacherservices.core.event;
-
-import lombok.Data;
-
-@Data
-public class StudentDeletedEvent {
-    private String studentId;
-}
--- student-services/src/main/java/com/example/teacherservices/query/FindStudentQuery.java
+++
@@ -1,4 +0,0 @@
-package com.example.teacherservices.query;
-
-public class FindStudentQuery {
-}
