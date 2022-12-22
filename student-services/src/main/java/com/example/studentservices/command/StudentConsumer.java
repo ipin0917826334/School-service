8f3908c3e71351b1ba6c207d79ea836c9b2909a7
Thitiwut85 <73686921+Thitiwut85@users.noreply.github.com>
2022-12-23 01:01:13 +0700

    Add files via upload

--- student-services/src/main/java/com/example/studentservices/command/StudentConsumer.java
+++ student-services/src/main/java/com/example/studentservices/command/StudentConsumer.java
@@ -1,5 +1,6 @@
 package com.example.studentservices.command;
 
+import com.example.studentservices.query.rest.GradeModel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,16 @@ import java.util.List;
 public class StudentConsumer {
     @RabbitListener(queues = "Queue2")
     public void TeacherConsumer(byte[] bytes) throws IOException {
-        List<String> grade = new ArrayList<String>();
+        List<String> grade = new ArrayList<>();
         ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
         DataInputStream in = new DataInputStream(bais);
         while (in.available() > 0) {
             String element = in.readUTF();
             grade.add(element);
         }
+        GradeModel gradeStudent = new GradeModel();
+        gradeStudent.setGrade(grade);
         System.out.println(grade);
+        System.out.println(gradeStudent);
     }
 }
---
+++ student-services/src/main/java/com/example/studentservices/query/rest/GradeModel.java
@@ -0,0 +1,10 @@
+package com.example.studentservices.query.rest;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GradeModel {
+    private List<String> grade;
+}
